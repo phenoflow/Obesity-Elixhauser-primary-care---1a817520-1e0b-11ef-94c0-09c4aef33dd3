90f54ff441b02fded8c7e986df373839d2d1daf5
Martin Chapman <contact@martinchapman.co.uk>
2024-05-29 23:32:38 +0100

    Update made by Phenoflow

---
+++ python/obesity-elixhauser-primary-care---primary.py
@@ -0,0 +1,25 @@
+# David Metcalfe, James Masters, Antonella Delmestri, Andrew Judge, Daniel Perry, Cheryl Zogg, Belinda Gabbe, Matthew Costa, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"C380800","system":"readv2"},{"code":"C380300","system":"readv2"},{"code":"C380100","system":"readv2"},{"code":"C380700","system":"readv2"},{"code":"C380400","system":"readv2"},{"code":"66C4.00","system":"readv2"},{"code":"C380500","system":"readv2"},{"code":"9OK..11","system":"readv2"},{"code":"L161.12","system":"readv2"},{"code":"C380000","system":"readv2"},{"code":"66CE.00","system":"readv2"},{"code":"C38z000","system":"readv2"},{"code":"C380600","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('obesity-elixhauser-primary-care-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["obesity-elixhauser-primary-care---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["obesity-elixhauser-primary-care---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["obesity-elixhauser-primary-care---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
